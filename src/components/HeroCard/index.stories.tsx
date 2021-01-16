82606d5a11e342d4d00f9af2d5a0ccf1d238259f
KEEN <kyh0927@live.co.kr>
2021-01-17 01:05:06 +0900

    feat : grid pattern test

--- src/components/HeroCard/index.stories.tsx
+++ src/components/HeroCard/index.stories.tsx
@@ -5,9 +5,48 @@ export default {
   title: 'Animation/Hero_Swiper'
 } as Meta;
 
-const HeroCardBlock = styled.section`
-  border: 1px solid red;
-`;
+const Emotion = {
+  Wrapper: styled.section`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-auto-rows: minmax(100px, auto);
+    grid-template-areas:
+      'hd hd hd hd hd'
+      'sd sd main main main'
+      'sd sd ft ft ft';
+    border: 1px solid red;
+  `,
+  Header: styled.header`
+    grid-area: hd;
+    background: pink;
 
-const Template = () => <HeroCardBlock>123</HeroCardBlock>;
+    h1 {
+      border: 2px solid blue;
+    }
+  `,
+  Side: styled.article`
+    grid-area: sd;
+    background: skyblue;
+  `,
+  Main: styled.article`
+    grid-area: main;
+    background: plum;
+  `,
+  Footer: styled.footer`
+    grid-area: ft;
+    background: yellowgreen;
+  `
+};
+
+const Template = () => (
+  <Emotion.Wrapper>
+    <Emotion.Header>
+      이모션 헤더
+      <h1>ㅋㅋㅋ</h1>
+    </Emotion.Header>
+    <Emotion.Side>이모션 사이드</Emotion.Side>
+    <Emotion.Main>이모션 사이드</Emotion.Main>
+    <Emotion.Footer>이모션 사이드</Emotion.Footer>
+  </Emotion.Wrapper>
+);
 export const Default = Template.bind({});
